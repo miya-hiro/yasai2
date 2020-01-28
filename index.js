a6e7fd82024c8e158f55ad32c1beecac2f8af069
miya-hiro <hiro.noon3@gmail.com>
2020-01-29 08:57:52 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminGifsicle = require('imagemin-gifsicle');
+const imageminSvgo = require('imagemin-svgo');
+
+imagemin(['img/*.{jpg,png,gif,svg}'], {
+  destination: 'bist/img/',
+  plugins: [
+    imageminMozjpeg({ quality: 80 }),
+    imageminPngquant({ quality: [0.6, 0.8] }),
+    imageminGifsicle(),
+    imageminSvgo()
+  ]
+}).then(() => {
+  console.log('Images optimized！');
+});
